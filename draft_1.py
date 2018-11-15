9a739ca70b8711107b7add918b4bf318eab67f49
sahmed-97 <40176168+sahmed-97@users.noreply.github.com>
2018-11-15 16:23:27 -0500

    Add files via upload

---
+++ draft_1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 19:22:32 2018
+
+@author: sheel
+"""
+
+import cv2
+import numpy as np
+import pandas as pd
+import av
+import time
+import math
+import os
+import csv
+import sys
+
+
+#%% Detect features between images through SIFT, ORB, SURF
+def feature_detection(img1, img2, detection_method = 'ORB'):
+    
+    
+    return detected_img
+
+
+
+#%%reading the video file
+    
